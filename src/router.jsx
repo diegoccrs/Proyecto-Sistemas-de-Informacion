c0d3d96dc76062cc3aa38d4aefb2b8f2bd8b47a0
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-06-21 03:30:19 -0400

    Edición en el Router

---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './views/HomePage';
+import LoginPage from './views/LoginPage';
+import RegisterPage from './views/RegisterPage';
+
+export const router = createBrowserRouter{[
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
+        path: '/register',
+        element: <RegisterPage />,
+    },
+]}
