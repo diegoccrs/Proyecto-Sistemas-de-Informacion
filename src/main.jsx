1d6d1be65fec3283e6de80724700be6d85069a4b
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-06-21 03:46:39 -0400

    Edición en el Main

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import UserProvider from './providers/UserProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      
       <App />
     </UserProvider>
   </React.StrictMode>,
