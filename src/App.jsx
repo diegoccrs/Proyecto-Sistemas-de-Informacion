c4ca6e4956798f80a8e7a8993fc23e9535a5d091
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-06-24 15:49:53 -0400

    Modificaciones en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+
+import { useState, useEffect } from 'react';
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut
+} from 'firebase/auth'
+import { auth } from './firebase.js'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [regEmail, setRegEmail] = useState('');
+  const [regPassword, setRegPassword] = useState('');
+  const [logEmail, setLogEmail] = useState('');
+  const [logPassword, setLogPassword] = useState('');
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+      onAuthStateChanged(auth, (currentUser) => {
+          setUser(currentUser);
+      })
+  }, []);
+
+  const register = async () => {
+      try {
+          const user = await createUserWithEmailAndPassword(
+              auth,
+              regEmail,
+              regPassword);
+
+          console.log(user);
+      } catch (error) {
+          console.log(error.message);
+      }
+  };
+
+  const login = async () => {
+      try {
+          const user = await signInWithEmailAndPassword(
+              auth,
+              logEmail,
+              logPassword);
+
+          console.log(user);
+      } catch (error) {
+          console.log(error.message);
+      }
+  };
+
+  const logout = async () => {
+
+      await signOut(auth);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <>
+      <div className='App'>
+          <h1>DELI PERNIL</h1>
+
+
+          <h3>Sign Up</h3>
+          <input type="email" placeholder='e-mail' onChange={(event) => {
+              setRegEmail(event.target.value)
+          }} />
+          <br />
+          <input type="password" placeholder='password' onChange={(event) => {
+              setRegPassword(event.target.value)
+          }} />
+          <br />
+          <br />
+          <button onClick={register}>Sign Up</button>
+
+
+          <br /><br /><br /><br />
+
+
+          <h3>Sign In</h3>
+          <input type="email" placeholder='e-mail' onChange={(event) => {
+              setLogEmail(event.target.value)
+          }} />
+          <br />
+          <input type="password" placeholder='password' onChange={(event) => {
+              setLogPassword(event.target.value)
+          }} />
+          <br />
+          <br />
+          <button onClick={login}>Sign In</button>
+
+
+          <br /><br /><br /><br />
+
+
+          <h3>User Logged In: {user?.email || 'None'}</h3>
+
+          <button onClick={logout}>Sign Out</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  </>
   )
 }
 
