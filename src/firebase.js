d73d181849f64b5c4304ea113136f768f421965b
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-06-24 15:53:09 -0400

    Modificaciones en FireBase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
+//import { getAnalytics } from "firebase/analytics";
+//import { getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 import {} from 'firebase/storage';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,3 +27,5 @@ export const db = getFirestore(app);
 
 export const auth = getAuth(app);
 //const analytics = getAnalytics(app);
+
+export const googleProvider = new GoogleAuthProvider();
