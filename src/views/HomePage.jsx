bc4065389c36185c93c5676b9dddafa06ec55f44
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-06-25 01:26:47 -0400

    Modificaciones Views

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,6 +1,18 @@
 
+import React from 'react'
+
+function HomePage() {
+    return (
+    <div>HomePage</div>
+    );
+};
+
+export default HomePage
+
+/**
 export default function HomePage() {
     const admins = useAdm();
 }
 
+**/
 
