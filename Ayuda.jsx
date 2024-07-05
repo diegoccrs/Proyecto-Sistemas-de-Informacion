9be816849a9abc680fb902681a1f168e15b13670
VirginiaSTR <71676092+VirginiaSTR@users.noreply.github.com>
2024-07-05 01:20:59 -0400

    Modificaciones en Ayuda

--- Ayuda.jsx
+++ Ayuda.jsx
@@ -1,11 +1,10 @@
 import styles from './Ayuda.module.css';
-//import c1 from '../img/View.png';
-//import local from '../img/iglogo.png';
+import c1 from '../img/View.png';
+import local from '../img/iglogo.png';
 import iglogo from '../img/iglogo.png';
 import xlogo from '../img/xlogo.png';
-//import fondo from '../img/fondo1.png';
-import map from '../img/Map.png';
-
+import fondo from '../img/fondo1.png';
+import Map from '../img/Map.png';
 
 function Ayuda() {
     
@@ -21,29 +20,41 @@ function Ayuda() {
                     <div className={styles.formularioInfo}>
                         <div className={styles.formulario1}>
                             <h3>Nombre</h3>
-                            <input type="text" />
+                            <input type="text" placeholder='Nombre'onChange={(event) => {
+                            setNombre(event.target.value)
+                            }}/>
+
                             <h3>Apellido</h3>
-                            <input type="text"/>
+                            <input type="text" placeholder='Apellido' onChange={(event) => {
+                            setApellido(event.target.value)
+                            }}/>
+
                         </div>
                             
                         <div className={styles.formulario2}>
                             <h3>Correo</h3>
-                            <input type="text"/>
+                            <input type="email" placeholder='Correo electrónico' onChange={(event) => {
+                            setRegEmail(event.target.value)
+                        }} />
                             <h3>Teléfono</h3>
-                            <input type="text"  />
+                            <input type="number" placeholder='Teléfono' onChange={(event) => {
+                            setTelefono(event.target.value)
+                        }} />
                         </div>
                     </div>
                     
                     <h3>Cuéntanos tus dudas o inquietudes</h3>
-                        <input className={styles.inputting} type="text"/>
+                        <input className={styles.inputting} type="text" placeholder='Dudas o Inquietudes' onChange={(event) => {
+                            setDudas(event.target.value)
+                            }}/>
                     
                 </div>
                 <button className={styles.button}>Contáctame</button>
             </div>
 
-            <div className={styles.contactos} style={{ backgroundImage: `url(${map})` }}>                
+            <div className={styles.contactos} style={{ backgroundImage: `url(${Map})` }}>
                 <div className={styles.containerContactos}>
-                    <h1><span className={styles.colored}>Llámanos</span></h1>                
+                    <h1>Llámanos</h1>
                     <h2> <a href="https://maps.app.goo.gl/GTvtRsQVo77zFdKL8"> Caracas 1073, Miranda, Universidad Metropolitana de Caracas</a></h2>
                     <h2> <a href=""></a> delipernil@gmail.com</h2>
                     <h2> <a href="tel:04242285852">0424-2285852</a></h2>
