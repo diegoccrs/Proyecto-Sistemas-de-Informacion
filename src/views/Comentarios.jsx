3123d99b482b0a065608cc1debbb10746aafeae9
GiselleEsclasans <122279533+GiselleEsclasans@users.noreply.github.com>
2024-07-04 22:47:40 -0400

    Add files via upload

--- src/views/Comentarios.jsx
+++ src/views/Comentarios.jsx
@@ -5,13 +5,50 @@ import iglogo from '../img/iglogo.png';
 import xlogo from '../img/xlogo.png';
 import Map from '../img/Map.png';
 
+
+import { useState, useEffect } from 'react';
+import {  doc, setDoc, addDoc, updateDoc, deleteDoc, onSnapshot, getDocs, query, where } from 'firebase/firestore';
+
+import {  firestoreDB  } from '../firebase-config.js'
+
+
 function Ayuda() {
-    
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [opinion, setOpinion] = useState('');
+    const [esAnonimo, setEsAnonimo] = useState(false);
+
+
+    async function enviarFeedback() {
+        try {
+            let correoValor = correo;
+            
+             if (esAnonimo) {
+            correoValor = "anonimo";
+            }
+            const docRef = doc(firestoreDB, "FeedBack", correoValor); 
+            await setDoc(docRef, {
+                nombreCompleto: nombre + " " + apellido,
+                email: correoValor,
+                comentario: opinion,
+                telefono: telefono,
+                anonimo: esAnonimo,
+            });
+            console.log("Document written with ID: ", correo);
+            setNombre('');
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+
+ 
 
     return (
         <div>
             <div className= {styles.slogan}>
-                <h1>Dejanos tu reseña</h1>
+                <h1 className= {styles.slogant}>Dejanos tu reseña</h1>
             </div>
             <div className={styles.formularioayuda }>
                 <h2>RELLENA LAS CASILLAS CON TUS DATOS</h2>
@@ -19,24 +56,36 @@ function Ayuda() {
                     <div className={styles.formularioInfo}>
                         <div className={styles.formulario1}>
                             <h3>Nombre</h3>
-                            <input type="text" />
+                            <input type="text" value={nombre} onChange={e => setNombre(e.target.value)}/>
                             <h3>Apellido</h3>
-                            <input type="text"/>
+                            <input type="text"value={apellido} onChange={e => setApellido(e.target.value)}/>
                         </div>
                             
                         <div className={styles.formulario2}>
                             <h3>Correo</h3>
-                            <input type="text"/>
+                            <input type="text" value={correo} onChange={e => setCorreo(e.target.value)} />
                             <h3>Teléfono</h3>
-                            <input type="text"  />
+                            <input type="text" value={telefono} onChange={e => setTelefono(e.target.value)}  />
+                            
+
                         </div>
+                        
+                       
                     </div>
+                    <div className={styles.anonimo}>
+                                <h3>Anónimo?</h3>
+                                <input
+                                type="checkbox"
+                                checked={esAnonimo}
+                                onChange={(e) => setEsAnonimo(e.target.checked)}
+                                />
+                            </div>
                     
                     <h3>DINOS LO QUE PIENSAS</h3>
-                        <input className={styles.inputting} type="text"/>
+                        <input className={styles.inputting} type="text"alue={opinion} onChange={e => setOpinion(e.target.value)} />
                     
                 </div>
-                <button className={styles.button}>Enviar</button>
+                <button className={styles.button} onClick={enviarFeedback}>Enviar</button>
             </div>
 
             <div className={styles.contactos} style={{ backgroundImage: `url(${Map})` }}>
--- src/views/Perfil.jsx
+++ src/views/Perfil.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import iglogo from '../img/iglogo.png';
 import { useNavigate, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import {
@@ -20,122 +21,172 @@ import { db } from '../firebase';
 import { doc, deleteDoc } from "firebase/firestore";
 
 
-function Nosotros() {
-
+function Perfil() {
+    const isAdmin = localStorage.getItem('admin') === 'true' ? true : false;
     const [user, setUser] = useState({});
-
+  
     useEffect(() => {
-        onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        })
+      onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+      });
     }, []);
-    
+  
     const logout = async () => {
-        
-        await signOut(auth);
-
-        localStorage.removeItem("admin");
-        localStorage.removeItem("email");
-        localStorage.removeItem("nombreCompleto");
-        localStorage.removeItem("telefono");
-        localStorage.removeItem("facultad");
-
-        navigate("/");
-
-        scroll(0, 0);
-        location.reload();
+      await signOut(auth);
+  
+      // Remover todos los items del localStorage
+      Object.keys(localStorage).forEach((key) => {
+        localStorage.removeItem(key);
+      });
+  
+      navigate('/');
+  
+      scroll(0, 0);
+      location.reload();
     };
-    
+  
     const navigate = useNavigate();
-
-
+  
     async function handleDeleteAndSignOut() {
-        try {
-            await deleteClient(); 
-    
-            const auth = getAuth();
-            await deleteUser(auth.currentUser).then(() => {
-           
-                console.log('User deleted');
-            }).catch((error) => {
-          
-                console.error("Error deleting user: ", error);
-
-            });
-            await signOut(auth); 
-        } catch (error) {
-            console.error("Error in handleDeleteAndSignOut: ", error);
-        }
-    }
-
-    async function deleteClient(){
-        try {
-          const id = auth.currentUser.uid;
-          const docRef = doc(db, "Usuario", id);
-            
-          await deleteDoc(docRef);
-
-        } catch (error) {
-          console.error("Error updating document: ", error);
-        }
+      try {
+        await deleteClient();
+  
+        const authInstance = getAuth();
+        await deleteUser(authInstance.currentUser).then(() => {
+          console.log('User deleted');
+        }).catch((error) => {
+          console.error("Error deleting user: ", error);
+        });
+        await signOut(authInstance);
+      } catch (error) {
+        console.error("Error in handleDeleteAndSignOut: ", error);
       }
-
-    
-
-
+    }
+  
+    async function deleteClient() {
+      try {
+        const id = auth.currentUser.uid;
+        const docRef = doc(db, "Usuario", id);
+  
+        await deleteDoc(docRef);
+      } catch (error) {
+        console.error("Error updating document: ", error);
+      }
+    }
+  
     return (
-        <div>
-            {!user ?
-            navigate("/acceder")
-            : <div className={styles.pageContainer}>
-                <div className={styles.boxContainer}>
-                    <div className={styles.titleContainer}>
-                        <h2>Perfil</h2>
-                    </div>
-                    <div className={styles.container}>
-                        <div className={styles.column}>
+      <div>
+        {!user ? (
+          navigate('/acceder')
+        ) : (
+          <div className={styles.pageContainer}>
+            <div className={styles.boxContainer}>
+              <div className={styles.titleContainer}>
+                <h2>Perfil</h2>
+              </div>
+              <div className={styles.container}>
+                <div className={styles.column}>
+                  {isAdmin && (
+                    <>
+                      <h2>Nombre</h2>
+                      <input
+                        type="text"
+                        placeholder="Nombre"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
+  
+                      <h2>Apellido</h2>
+                      <input
+                        type="text"
+                        placeholder="Apellido"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
 
-                            <h2>Nombre</h2>
-                            <input type="text" placeholder='Nombre' onChange={(event) => {
-                                
-                            }} />
+                        <Link className={styles['nav-link']} to="/menuadmin">
+                            Regresar
+                        </Link>
+                    </>
+                  )}
+  
+                  {!isAdmin && (
+                    <>
+                      <h2>Nombre</h2>
+                      <input
+                        type="text"
+                        placeholder="Nombre"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
+  
+                      <h2>Apellido</h2>
+                      <input
+                        type="text"
+                        placeholder="Apellido"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
+  
+                      <h2>Facultad</h2>
+                      <input
+                        type="text"
+                        placeholder="Facultad"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
+  
+                      <h2>Teléfono</h2>
+                      <input
+                        type="number"
+                        placeholder="Teléfono"
+                        onChange={(event) => {
+                          
+                        }}
+                      />
 
-                            <h2>Apellido</h2>
-                            <input type="text" placeholder='Apellido' onChange={(event) => {
-                                
-                            }} />
+                       
+                        
+                        <img className={styles.perfil} src={iglogo} alt="Logo" />
+                        <h2>Foto de Perfil</h2>
 
-                            <button className={styles.button} onClick={logout}>
-                                Cerrar Sesión
-                            </button>
+                        <Link className={styles['nav-link']} to="/">
+                            Regresar
+                        </Link>
+                        <Link className={styles['nav-link']} to="/perfil/editarperfil">
+                            Editar
+                        </Link>
+                        <Link
+                            className={styles['nav-link']}
+                            to="/"
+                            onClick={handleDeleteAndSignOut}
+                        >
+                            Eliminar
+                        </Link>
 
-                        </div>
-                        <div className={styles.column}>
-
-                            <h2>Facultad</h2>
-                            <input type="text" placeholder='Facultad' onChange={(event) => {
-                                
-                            }} />
-
-                            <h2>Teléfono</h2>
-                            <input type="number" placeholder='Teléfono' onChange={(event) => {
-                              
-                              
-                             
-                            }} />
-
-                            <Link className={styles["nav-link"]} to="/">Regresar</Link>
-                              <Link className={styles["nav-link"]} to="/perfil/editarperfil">Editar</Link>
-                              <Link className={styles["nav-link"]} to="/" onClick={handleDeleteAndSignOut}>Eliminar</Link>  
-                        </div>
-                    </div>
+                    </>
+                  )}
+  
+                  <button className={styles.button} onClick={logout}>
+                    Cerrar Sesión
+                  </button>
                 </div>
+                <div className={styles.column}>
+                  
+                 
+                  
+                </div>
+              </div>
             </div>
-            }
-        </div>
+          </div>
+        )}
+      </div>
     );
-}
-
-
-
-export default Nosotros
+  }
+  
+  export default Perfil;
